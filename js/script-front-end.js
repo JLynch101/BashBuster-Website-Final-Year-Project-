cc5a5a1a529a348ef5f6b22b6a66c3be68883c1e
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-04-30 17:17:10 +0100

    Website

---
+++ js/script-front-end.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function(){
+    if( jQuery("#wppb-register-user").length ) {
+        jQuery('#wppb-register-user').on('submit', function (e) {
+            //stop submitting the form to see the disabled button effect
+            e.preventDefault();
+            //disable the submit button
+            jQuery('.form-submit #register').attr('disabled', true);
+            this.submit();
+        });
+    }
+
+    //scroll to top on success message
+    if( jQuery("#wppb_form_general_message").length ){
+        jQuery([document.documentElement, document.body]).animate({ scrollTop: jQuery("#wppb_form_general_message").offset().top }, 500);
+    }
+});
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
